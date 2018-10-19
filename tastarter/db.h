33fd64bc7ddcfeb064b8f72c2ad14adbe76b2446
Sushrut Shringarputale <contact@sushshring.com>
2018-10-18 21:21:33 -0400

    Added some self review documentation to db.h

--- tastarter/db.h
+++ tastarter/db.h
@@ -11,17 +11,26 @@
 FILE *file;
 
 char *filename = "names.txt";
-/**
- * Retrieves a value from the database
- * @param key
- * @param ret_buf
- * @param ret_len
- * @return
- */
  inline void db_init() {
    file = fopen(filename, "r+");
  }
+/**
+ * Retrieves a value from the database
+ * @param key The key of the object
+ * @param ret_buf A buffer that will be allocated by the function and the data returned in
+ * @param ret_len int * The length of the returned data
+ * @return 0 if success 1 if failure
+ */
 int db_get(char *key, char **ret_buf, int *ret_len);
+
+/**
+ * Adds a value to the database. If the key exists, it will replace it
+ * @param key The key of the object
+ * @param value The value to associate with the key
+ * @param ret_buf A buffer that will be allocated by the function and the data returned in
+ * @param ret_len int * The length of the returned data
+ * @return 0 if success 1 if failure
+ */
 int db_put(char *key, char *value, char **ret_buf, int *ret_len);
 
 #endif //P1_CSRF_DB_H
