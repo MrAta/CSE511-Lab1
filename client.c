6e9a77d8832dd1a31cc9f74024ccb30646cea9e3
Ata Fatahi <immrata@gmail.com>
2018-10-29 00:48:33 -0400

    client finished

--- client.c
+++ client.c
@@ -8,12 +8,16 @@
 #include <pthread.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 #define PORT 8085
-#define N_KEY 1000 //number of unique keys
-#define a 1.25 //parameter for zipf distribution
+#define N_KEY 1000 //total number of unique keys
+#define a 1 //parameter for zipf distribution
 #define ratio 0.1 // get/put ratio
 #define key_size 16
 #define value_size 84
+#define MAX_ENTRY_SIZE 11264
+#define NUM_THREADS 25
+#define NUM_OPS //total number of operation for workload
 long int zeta; //zeta fixed for zipf
 char * keys[N_KEY] = {NULL};
 // char * values[N_KEY] = {NULL};
@@ -21,20 +25,26 @@ double popularities[N_KEY];
 double cdf[N_KEY];
 int arr_rate = 100;
 int arr_type = 1;
+struct sockaddr_in *serv_addr;
+pthread_mutex_t fp_mutex;
+FILE *fp;
 
 static char *rand_string(char *str, size_t size)
 {
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    if (size) {
-        --size;
-        for (size_t n = 0; n < size; n++) {
-            int key = rand() % (int) (sizeof charset - 1);
-            str[n] = charset[key];
-        }
-        str[size] = '\0';
-    }
-    return str;
+  int seed = time(NULL);
+  srand(seed);
+  const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  if (size) {
+      --size;
+      for (size_t n = 0; n < size; n++) {
+          int key = rand() % (int) (sizeof charset - 1);
+          str[n] = charset[key];
+      }
+      str[size] = '\0';
+  }
+  return str;
 }
+
 char* rand_string_alloc(size_t size)
 {
      char *s = malloc(size + 1);
@@ -56,19 +66,19 @@ void generate_key_values(){
   for(int i=0; i< N_KEY; i++){
     char * _key = rand_string_alloc(key_size);
     keys[i] = _key;
-    // char * _value = rand_string_alloc(value_size);
-    // values[i] = _value;
-    //TODO: do we need to write them in a file?
   }
 }
+
 double zipf(int x){
   return (1/(pow(x,a)))/zeta;
 }
+
 void generate_popularities(){
   for(int i=0; i < N_KEY; i++){
     popularities[i] = zipf(i+1);
   }
 }
+
 void calc_cdf(){
   cdf[0] = popularities[0];
   for(int i=1; i < N_KEY; i++){
@@ -77,41 +87,43 @@ void calc_cdf(){
 }
 
 char * next_key(){
+  int seed = time(NULL);
+  srand(seed);
   double p = (rand() / (RAND_MAX+1.0))*cdf[N_KEY - 1];
   //TODO: optimize it with binary search
   for(int i=0; i< N_KEY; i++)
-    if(p < cdf[i]){
+    if(p <= cdf[i]){
       return keys[i];
     }
     return NULL;
 }
 
 double nextArrival(){
-    if (arr_type == 1){//unifrom
-      return 1/arr_rate;
-    }
-    else{//exponential
-      return (-1 * log(1 - (rand()/(RAND_MAX+1.0)))/(arr_rate));
-    }
-    return 1/arr_rate; //default: unifrom :D
+  int seed = time(NULL);
+  srand(seed);
+  if (arr_type == 1){//unifrom
+    return 1/arr_rate;
+  }
+  else{//exponential
+    return (-1 * log(1 - (rand()/(RAND_MAX+1.0)))/(arr_rate));
+  }
+  return 1/arr_rate; //default: unifrom :D
 }
 
 int nextReqType(){
+  int seed = time(NULL);
+  srand(seed);
   double p = (rand() / (RAND_MAX+1.0));
   //req types: 0 indicates get requst, 1 indicates put request
   if(p <= ratio) return 0;
   return 1;
 
 }
-char *hello[] = {"GET Tammy"};//{"GET Atajoon", "GET Clifton", "GET Tammy", "INSERT Sami Pali", "DELETE Tammy", "GET Tammy"};
 
-
-struct sockaddr_in *serv_addr;
-
-void *client_func() {
+void write_all_keys(){
   int sock = 0, valread;
 
-  char buffer[1024] = {0};
+  char buffer[MAX_ENTRY_SIZE] = {0};
 
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
@@ -124,36 +136,100 @@ void *client_func() {
       printf("\nConnection Failed \n");
       exit(0);
   }
-for(int i=0; i < 6; i++){
-  clock_t t;
-  t = clock();
-  send(sock , hello[0] , strlen(hello[0]) , 0 );
-  printf("REQUEST SENT: %s\n", hello[0]);
-  valread = read( sock , buffer, 1024);
-  t = clock() - t;
-  double time_taken = ((double)t)/CLOCKS_PER_SEC;
-  printf("RESPONSE: %s took %f\n",buffer,  time_taken);
+  //To keep the system in steady state, we should have most popular keys in cache.
+  //Therefor, we first write less popular keys and then popular keys to fill the cache
+  for(int i=N_KEY -1; i >=0 ; i--){
+    char cmd[MAX_ENTRY_SIZE];
+    char * _cmd = "INSERT ";
+    char *key = keys[i];
+    char * s = " ";
+    char *val = rand_string_alloc(value_size);
+    strcat(cmd, _cmd);
+    strcat(cmd, key);
+    strcat(cmd, s);
+    strcat(cmd, val);
+
+    send(sock , cmd , strlen(cmd) , 0 );
+    valread = read( sock , buffer, 1024);
+    if(valread > 0){
+      printf("Inserted %d key(s).\n", N_KEY - i + 1);
+      }
+    }
 }
 
+void *client_func() {
+  int sock = 0, valread;
+
+  char buffer[MAX_ENTRY_SIZE] = {0};
+
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+      printf("\n Socket creation error \n");
+      exit(0);
+  }
+
+  if (connect(sock, (struct sockaddr *)serv_addr, sizeof(struct sockaddr_in)) < 0)
+  {
+      printf("\nConnection Failed \n");
+      exit(0);
+  }
+  int local_count = 0;
+  while (local_count <= (int)NUM_OPS/NUM_THREADS) {
+
+    char cmd[MAX_ENTRY_SIZE];
+
+    if(nextReqType() == 0)
+      _cmd = "GET ";
+    else
+      _cmd = "PUT ";
+
+    char *key = next_key();
+    char * s = " ";
+    char *val = rand_string_alloc(value_size);
+
+    strcat(cmd, _cmd);
+    strcat(cmd, key);
+    strcat(cmd, s);
+    strcat(cmd, val);
+
+    clock_t t;
+    t = clock();
+    send(sock, cmd, strlen(cmd), 0);
+    valread = read(sock, buffer, MAX_ENTRY_SIZE);
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    pthread_mutex_lock(&fp_mutex);
+    fprintf(fp, "%f\n", time_taken);
+    pthread_mutex_unlock(&fp_mutex);
+    // printf("SENT %s || RESPONSE: buffer\n", cmd);
+    printf("Sent %d requsts so far\n", local_count+1);
+    usleep(nextArrival()*1000000*NUM_THREADS);
+    local_count++;
+  }
 }
 
 int main(int argc, char const *argv[])
 {
+    fp = fopen("response_time.log", "w");
+    pthread_mutex_init(&fp_mutex, 0);
+    printf("Generating Keys...\n");
     generate_key_values();
+    printf("Generated Keys.\n");
     zeta = calc_zeta();
+    printf("Generating popularities...\n");
     generate_popularities();
     calc_cdf();
-    printf("first: %f and last: %f", cdf[0], cdf[N_KEY -1] );
-    for(int i=0; i < 15; i++)
-      printf("%s\n", next_key());
-    pthread_t client_thread[10];
+    printf("Generated popularities.\n");
 
     serv_addr = (struct sockaddr_in*) malloc (sizeof(struct sockaddr_in));
-
     memset(serv_addr, '0', sizeof(serv_addr));
 
     serv_addr->sin_family = AF_INET;
     serv_addr->sin_port = htons(PORT);
+
+    printf("Writing All the keys...\n", );
+    write_all_keys();
+    printf("All keys are written.\n", );
 
     // Convert IPv4 and IPv6 addresses from text to binary form
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr->sin_addr)<=0)
@@ -162,11 +238,18 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
-    // for(int i=0; i<10; i++)
-    client_func();
-    // pthread_create(&client_thread[i], NULL, client_func, NULL);
-// for(int i=0; i<10; i++)
-    // pthread_join(client_thread[i], NULL);
-
+    printf("Starting running the workload...\n" );
+    clock_t _t;
+    _t = clock();
+    pthread_t client_thread[NUM_THREADS];
+    for(int i=0; i<NUM_THREADS; i++)
+      pthread_create(&client_thread[i], NULL, client_func, NULL);
+    for(int i=0; i<10; i++)
+      pthread_join(client_thread[i], NULL);
+    _t = clock();
+    double _time_taken = ((double)_t)/CLOCKS_PER_SEC;
+    printf("Finished running the workload.\n");
+    printf("workload took %f\n",_time_taken );
+    fclose(fp);
     return 0;
 }
