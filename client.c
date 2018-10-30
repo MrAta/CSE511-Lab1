4b61feee55547ce1816719dcbe79755d0570e930
Ata Fatahi Baarzi <immrata@gmail.com>
2018-10-29 21:36:35 -0400

    no need for decreamnet

--- client.c
+++ client.c
@@ -36,7 +36,7 @@ static char *rand_string(char *str, size_t size)
   // srand(seed);
   const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   if (size) {
-      --size;
+      //--size;
       for (size_t n = 0; n < size; n++) {
           int key = rand() % (int) (sizeof charset - 1);
           str[n] = charset[key];
